4ba78c0ce2eb6242d225dd22a02782bd7fdf73c2
Dhruvil Kapadiya <136882343+dhruvil931@users.noreply.github.com>
2026-04-26 21:11:16 +0530

    Solved Two Sum

---
+++ Arrays/Day 3/two-sum.cpp
@@ -0,0 +1,27 @@
+// Problem: Two Sum
+// Source: Leetcode
+// Approach: Hashmap
+// Time: O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& arr, int tar) {
+        unordered_map<int, int> m;
+        vector<int> ans;
+
+        for(int i=0; i<arr.size(); i++) {
+            int first = arr[i];
+            int second = tar - first;
+
+            if(m.find(second) != m.end()) {
+                ans.push_back(i);
+                ans.push_back(m[second]);
+            }
+
+            m[first] = i;
+        }
+
+        return ans;
+    }
+};
