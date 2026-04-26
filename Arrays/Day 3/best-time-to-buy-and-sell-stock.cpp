25a2f24f23f2fb0cdf6b38ed2cdd8d1ea39181a3
Dhruvil Kapadiya <136882343+dhruvil931@users.noreply.github.com>
2026-04-26 21:31:17 +0530

    Solved Best Time to Buy and Sell Stock

---
+++ Arrays/Day 3/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,21 @@
+// Problem: Best Time to Buy and Sell Stock
+// Source: Leetcode
+// Approach: Greedy Approch
+// Time: O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int bestBuy = prices[0];
+        int maxProfit = 0;
+
+        for(int i = 0; i < prices.size(); i++) {
+            if(prices[i] > bestBuy) {
+                maxProfit = max(maxProfit, prices[i] - bestBuy);
+            }
+            bestBuy = min(bestBuy, prices[i]);
+        }
+        return maxProfit;
+    }
+};
