aaac8b3cf0d41d2a4efd63fd20b5e25d869f49ca
Dhruvil Kapadiya <136882343+dhruvil931@users.noreply.github.com>
2026-04-26 21:29:16 +0530

    Solved Maximum Subarray

---
+++ Arrays/Day 3/maximum-subarray.cpp
@@ -0,0 +1,23 @@
+// Problem: Maximum Subarray
+// Source: Leetcode
+// Approach: Kadane’s Algorithm
+// Time: O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = INT_MIN;
+        int currSum = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            currSum += nums[i];
+            maxSum = max(currSum, maxSum);
+
+            if(currSum < 0) {
+                currSum = 0;
+            }
+        }
+        return maxSum;
+    }
+};
