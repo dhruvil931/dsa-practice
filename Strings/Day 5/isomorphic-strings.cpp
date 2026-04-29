043a82cfe91adc583dd3b86a80cc226f460085e0
Dhruvil Kapadiya <136882343+dhruvil931@users.noreply.github.com>
2026-04-29 16:51:25 +0530

    Solved Isomorphic Strings

---
+++ Strings/Day 5/isomorphic-strings.cpp
@@ -0,0 +1,34 @@
+// Problem: Isomorphic Strings
+// Source: Leetcode
+// Approach: Two hash maps
+// Time: O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> map1, map2;
+
+        for(int i=0; i<s.size(); i++) {
+            if(map1.find(s[i]) != map1.end()) {
+                if(map1[s[i]] != t[i]) {
+                    return false;
+                }
+            }
+            else {
+                map1[s[i]] = t[i];
+            }
+
+            if(map2.find(t[i]) != map2.end()) {
+                if(map2[t[i]] != s[i]) {
+                    return false;
+                }
+            }
+            else {
+                map2[t[i]] = s[i];
+            }
+        }
+
+        return true;
+    }
+};
