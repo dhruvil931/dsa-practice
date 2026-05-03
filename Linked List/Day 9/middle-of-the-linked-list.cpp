e6e9cc704a8b45dd90175f8e5376cf9cf36bb121
Dhruvil Kapadiya <136882343+dhruvil931@users.noreply.github.com>
2026-05-03 23:29:38 +0530

    Solved Middle of the Linked List

---
+++ Linked List/Day 9/middle-of-the-linked-list.cpp
@@ -0,0 +1,24 @@
+// Problem: Middle of the Linked List
+// Source: Leetcode
+// Approach: Slow & fast pointers
+// Time: O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        if(head == NULL || head->next == NULL) {
+            return head;
+        }
+
+        while(fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+};
