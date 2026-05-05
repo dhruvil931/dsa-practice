3b3f63c354e1c01777884e891abed4ae37ecbf5a
Dhruvil Kapadiya <136882343+dhruvil931@users.noreply.github.com>
2026-05-05 23:38:44 +0530

    Solved Add 1 to a Linked List Number

---
+++ Linked List/Day 11/add-1-to-a-number-represented-as-linked-list.cpp
@@ -0,0 +1,36 @@
+// Problem: Add 1 to a Linked List Number
+// Source: GFG
+// Approach: Recursion
+// Time: O(n)
+// Space: O(n)
+
+class Solution {
+  public:
+    int helper(Node* temp) {
+        if(temp == NULL) {
+            return 1;
+        }
+        
+        int carry = helper(temp->next);
+        
+        temp->data += carry;
+        
+        if(temp->data < 10) {
+            return 0;
+        }
+        
+        temp->data = 0;
+        return 1;
+    }
+  
+    Node* addOne(Node* head) {
+        int carry = helper(head);
+        
+        if(carry == 1) {
+            Node* newNode = new Node(1);
+            newNode->next = head;
+            head = newNode;
+        }
+        return head;
+    }
+};
