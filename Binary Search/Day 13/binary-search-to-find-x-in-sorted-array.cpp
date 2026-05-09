7b48b3c1dc5b3f47f53bf8be007563b0180b5251
Dhruvil Kapadiya <136882343+dhruvil931@users.noreply.github.com>
2026-05-09 16:57:25 +0530

    Solved Binary Search to find X in sorted array

---
+++ Binary Search/Day 13/binary-search-to-find-x-in-sorted-array.cpp
@@ -0,0 +1,30 @@
+// Problem: Binary Search to find X in sorted array
+// Source: Leetcode
+// Approach: Binary Search (compare target with middle element, reduce search space)
+// Time: O(log n)
+// Space: O(1)
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size()-1;
+
+        while(left <= right) {
+            int mid = left + (right-left)/2;
+
+            if(nums[mid] == target) {
+                return mid;
+            }
+
+            if(nums[mid] < target) {
+                left = mid+1;
+            }
+            else {
+                right = mid-1;
+            }
+        }
+
+        return -1;
+    }
+};
