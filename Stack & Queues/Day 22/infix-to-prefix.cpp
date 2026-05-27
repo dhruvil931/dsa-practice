2cbdfce85a7e09e5218b05d824d0aff6aa02df80
Dhruvil Kapadiya <136882343+dhruvil931@users.noreply.github.com>
2026-05-27 18:37:45 +0530

    Solved Infix To Prefix

---
+++ Stack & Queues/Day 22/infix-to-prefix.cpp
@@ -0,0 +1,96 @@
+// Problem: Infix To Prefix
+// Source: GFG
+// Approach: Stack + Reverse Traversal
+// Time: O(n)
+// Space: O(n)
+
+class Solution {
+  public:
+    int priority(char c) {
+        if(c == '^') {
+            return 3;
+        }
+        else if(c == '*' || c == '/') {
+            return 2;
+        }
+        else if(c == '+' || c == '-') {
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    }
+  
+    string infixToPrefix(string &s) {
+        reverse(s.begin(), s.end());
+        
+        int i = 0;
+        
+        while(i < s.size()) {
+            if(s[i] == '(') {
+                s[i] = ')';
+            }
+            else if(s[i] == ')') {
+                s[i] = '(';
+            }
+            i++;
+        }
+        
+        stack<char> st;
+        string ans = "";
+        
+        i = 0;
+        
+        while(i < s.size()) {
+            if
+            (    
+                s[i] >= 'A' && s[i] <= 'Z' ||
+                s[i] >= 'a' && s[i] <= 'z' ||
+                s[i] >= '0' && s[i] <= '9'
+            ) {
+                ans += s[i];
+            }
+            
+            else if(s[i] == '(') {
+                st.push(s[i]);
+            }
+            
+            else if(s[i] == ')') {
+                while(!st.empty() && st.top() != '(') {
+                    ans += st.top();
+                    st.pop();
+                }
+                
+                st.pop();
+            }
+            
+            else {
+                while(
+                    !st.empty() &&
+                    (
+                        priority(s[i]) < priority(st.top()) ||
+                        (
+                            priority(s[i]) == priority(st.top()) &&
+                            s[i] == '^'
+                        )
+                    )
+                ) {
+                    ans += st.top();
+                    st.pop();
+                }
+                st.push(s[i]);
+            }
+            
+            i++;
+        }
+        
+        while(!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+        
+        reverse(ans.begin(), ans.end());
+        
+        return ans;
+    }
+};
