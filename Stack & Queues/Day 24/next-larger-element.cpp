2550ea741f4be1f17a19cafd61a5062c069daba1
Dhruvil Kapadiya <136882343+dhruvil931@users.noreply.github.com>
2026-06-03 18:11:18 +0530

    Solved Next Greater Element

---
+++ Stack & Queues/Day 24/next-larger-element.cpp
@@ -0,0 +1,26 @@
+// Problem: Next Greater Element
+// Source: GFG
+// Approach: Monotonic Decreasing Stack
+// Time: O(n)
+// Space: O(n)
+
+class Solution {
+  public:
+    vector<int> nextLargerElement(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> ans(n);
+        stack<int> st;
+        
+        for(int i=n-1; i>=0; i--) {
+            while(!st.empty() && st.top() <= arr[i]) {
+                st.pop();
+            }
+            
+            ans[i] = st.empty() ? -1 : st.top();
+            
+            st.push(arr[i]);
+        }
+        
+        return ans;
+    }
+};
